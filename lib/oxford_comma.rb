d4af74885e80cddb6addd13ebc8d9dae6f127434
Terry Threatt <terry.threatt@gmail.com>
2020-02-18 03:58:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,5 @@
 require 'pry'
 
 def oxford_comma(array)
-  #binding.pry
-  if array == 1 
-    return "#{array[0]}"
-  else 
-    array.split("")
-    return array.join("and")
-  end
+  str = array.
 end
